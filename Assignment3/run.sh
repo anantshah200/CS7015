a83ebe305edc3e7ba018cba620d74d191c5870ef
anantshah200 <anantshah200@gmail.com>
2019-02-13 20:18:21 +0530

    Script to run

---
+++ Assignment3/run.sh
@@ -0,0 +1,1 @@
+python backprop.py --lr 0.01 --momentum 0.5 --num_hidden 3 --sizes 100,100,100 --activation sigmoid --loss sq --opt adam --batch_size 20 --epochs 5 --anneal true --save_dir pa1/ --expt_dir pa1/exp1/ --train train.csv --val validation.csv --test test.csv
